9cca3628046e021e9ca9a0076568d8062f7f735b
rattanaphong2544 <ratanapong999@gmail.com>
2026-02-27 11:46:28 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,3 @@
-// @ts-nocheck
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+import React from 'react';
+declare const App: React.ComponentType<any>;
+export default App;

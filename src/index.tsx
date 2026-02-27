ce31710c90b6b406e6502d86d1f4f10397c3419f
rattanaphong2544 <ratanapong999@gmail.com>
2026-02-27 11:35:31 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
+// @ts-nocheck
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}

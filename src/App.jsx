2abf1d6efdd818b3d1aa18f77360ff6327b6c4a8
rattanaphong2544 <ratanapong999@gmail.com>
2026-03-01 01:33:13 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -338,11 +338,11 @@ const RingVisualizer = ({ selectedPositions, onTogglePosition, ringKey }) => {
   const r = 130;
   const segments = [
     { id: "K", label: "K", start: -12.5, end: 12.5 },
-    { id: "C2", label: "C2", start: 12.5, end: 79.5 },
-    { id: "B2", label: "B2", start: 79.5, end: 146.5 },
+    { id: "C1", label: "C1", start: 12.5, end: 79.5 },
+    { id: "B1", label: "B1", start: 79.5, end: 146.5 },
     { id: "A", label: "A", start: 146.5, end: 213.5 },
-    { id: "B1", label: "B1", start: 213.5, end: 280.5 },
-    { id: "C1", label: "C1", start: 280.5, end: 347.5 },
+    { id: "B2", label: "B2, start: 213.5, end: 280.5 },
+    { id: "C2", label: "C2", start: 280.5, end: 347.5 },
   ];
   const rotation = (parseInt(ringKey) % 16) * 22.5;
 
